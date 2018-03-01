c9a1fc9d26ee93fc4376dc217151da911f9ee619
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-03-02 00:41:22 +0300

    Add files via upload

---
+++ EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskDev1
+{
+  class EntryPoint
+  {
+    static void Main(string[] symbolsString)
+    {
+      if (symbolsString.Length == 1)
+      {
+        Sequence sequence = new Sequence(symbolsString[0]);
+        Console.WriteLine(sequence.FindLongestSameSymbols());
+      }
+      else
+      {
+        Console.WriteLine("Wrong arguments.");
+      }
+    }
+  }
+}
+
