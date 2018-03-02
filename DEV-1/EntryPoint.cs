3bc773af491f7e6aada72a6853822d2996ee0ea6
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-03-02 11:07:02 +0300

    Update EntryPoint.cs

--- DEV-1/EntryPoint.cs
+++ DEV-1/EntryPoint.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace TaskDev1
 {
+  /// <summary>
+  /// This class for print information about
+  /// max length same symbols and inputed arguments command line.
+  /// </summary>
   class EntryPoint
   {
+    /// <summary>
+    /// This method print information.
+    /// </summary>
+    /// <param name="symbolsString">String which inputed in arguments command line.</param>
     static void Main(string[] symbolsString)
     {
       if (symbolsString.Length == 1)
