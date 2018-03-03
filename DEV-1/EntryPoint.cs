a55de70f700a4af0fe1508d06248c2a807130de2
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-03-03 12:34:10 +0300

    Update EntryPoint.cs

--- DEV-1/EntryPoint.cs
+++ DEV-1/EntryPoint.cs
@@ -1,23 +1,18 @@
 using System;
 
 namespace TaskDev1
-{
+{  
   /// <summary>
-  /// This class for print information about
-  /// max length same symbols and inputed arguments command line.
+  /// It's an entry point in the program.
   /// </summary>
   class EntryPoint
-  {
-    /// <summary>
-    /// This method print information.
-    /// </summary>
-    /// <param name="symbolsString">String which inputed in arguments command line.</param>
+  {    
     static void Main(string[] symbolsString)
     {
       if (symbolsString.Length == 1)
       {
-        SequenceOfString sequence = new SequenceOfString(symbolsString[0]);
-        Console.WriteLine(sequence.FindMaxLengthSameSymbols());
+        Counter counter = new Counter(symbolsString[0]);
+        Console.WriteLine(counter.FindMaxLengthSameSymbols());
       }
       else
       {
@@ -26,4 +21,3 @@ namespace TaskDev1
     }
   }
 }
-
