991978d1d70914fd9b18fbecb2533c919a8b52c1
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-03-16 10:09:58 +0300

    Add files via upload

---
+++ DEV-2/EvenIndexesString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+  /// <summary>
+  /// This class creates the string with with even indexes.
+  /// </summary>
+  public class EvenIndexesString
+  {
+    /// <summary>
+    /// This method creates the string with even indexes.
+    /// </summary>
+    /// <param name="currentString"></param>
+    /// <returns></returns>
+    public string CreateEvenIndexesString(string currentString)
+    {
+      StringBuilder result = new StringBuilder();
+      for (int i = 0; i < currentString.Length; i += 2)
+      {
+        result.Append(currentString[i]);
+      }
+      return result.ToString();
+    }    
+  }
+}
---
+++ DEV-2/EvenIndexesStringTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DEV_2.Test
+{
+  [TestClass]
+  public class EvenIndexesStringTest
+  {
+    [TestMethod]
+    public void UnEvenStringLengthTestPositive()
+    {
+      string currentString = "kovalchuk";
+      string expected = "kvlhk";
+      EvenIndexesString stringCreator = new EvenIndexesString();
+      string actual = stringCreator.CreateEvenIndexesString(currentString);
+      Assert.AreEqual(expected, actual);      
+    }
+
+    [TestMethod]
+    public void EvenStringLengthTestPositive()
+    {
+      string currentString = "kovalchukk";
+      string expected = "kvlhk";
+      EvenIndexesString stringCreator = new EvenIndexesString();
+      string actual = stringCreator.CreateEvenIndexesString(currentString);
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void NullStringLengthPositive()
+    {
+      string currentString = "";
+      string expected = "";
+      EvenIndexesString stringCreator = new EvenIndexesString();
+      string actual = stringCreator.CreateEvenIndexesString(currentString);
+      Assert.AreEqual(expected, actual);
+    }
+    [TestMethod]
+    public void OneStringLengthPositive()
+    {
+      string currentString = "a";
+      string expected = "a";
+      EvenIndexesString stringCreator = new EvenIndexesString();
+      string actual = stringCreator.CreateEvenIndexesString(currentString);
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
