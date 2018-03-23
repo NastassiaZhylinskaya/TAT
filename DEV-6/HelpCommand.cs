d4c571e6e7d0e46f31dcb27e0ca04f1a663eea28
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-03-23 15:07:08 +0300

    Add files via upload

---
+++ DEV-6/HelpCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DEV6.Commands
+{
+  /// <summary>
+  /// Child class of class Command. 
+  /// </summary>
+  public class HelpCommand : Command
+  {
+    /// <summary>
+    /// This method for help program.
+    /// </summary>
+    /// <param name="products">Empty products.</param>
+    /// <param name="arg">Empty parametr arg.</param>
+    public void execute(List<Product> products = null, string arg = null)
+    {
+      foreach (var command in StorageCommander.Commands)
+      {
+        Console.WriteLine(command);
+      }
+    }
+  }
+}
