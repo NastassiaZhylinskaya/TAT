8aab38f2f9b8f1e8e1ccceb69f2cc755608e3347
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-03-23 15:14:14 +0300

    Add files via upload

---
+++ DEV-6/ExitCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DEV6.Commands
+{
+  /// <summary>
+  /// Child class of class Command.
+  /// </summary>
+  public class ExitCommand : Command
+  {
+    private const string GOODBYE = "Goodbye!";
+    /// <summary>
+    /// This method for exit program.
+    /// </summary>
+    /// <param name="products">Empty products.</param>
+    /// <param name="arg">Empty parametr arg.</param>
+    public void execute(List<Product> products = null, string arg = null)
+    {
+      Console.WriteLine(GOODBYE);
+      Environment.Exit(0);
+    }
+  }
+}
