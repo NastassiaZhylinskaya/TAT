4987edd03758dd70c756672cd532dbc8c1e0284d
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-04-18 12:32:19 +0300

    Update PersonBuilderTest.cs

--- DEV-8.Test/PersonBuilderTest.cs
+++ DEV-8.Test/PersonBuilderTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DEV8.Test
@@ -6,9 +6,30 @@ namespace DEV8.Test
   [TestClass]
   public class PersonBuilderTest
   {
+    private const string FIRSTNAME = "firstName";
+    private const string LASTNAME = "lastName";
+    private const int AGE = 0;
+    private const string SEX = "sex";
+
+    public TestContext TestContext { get; set; }
+    private const string connectionStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\..\\DataSources\\PersonsBuilderTestsData.xlsx;Extended Properties=\"Excel 12.0 Xml;HDR=YES\";";
+
     [TestMethod]
-    public void TestMethod1()
+    [DataSource(connectionStr, "Ages$")]
+    [ExpectedException(typeof(Exception))]
+    public void Build_PersonWithInvalidAgetParameter_Exception()
     {
+      int age = Convert.ToInt32(TestContext.DataRow["Negative Age"]);
+      var res = new PersonsBuilder().Build(FIRSTNAME, LASTNAME, AGE, SEX);
     }
+
+    [TestMethod]
+    [DataSource(connectionStr, "Ages$")]
+    public void Build_PersonWithValidSexParameter_IsPerson()
+    {
+      int age = Convert.ToInt32(TestContext.DataRow["Exception Age"]);
+      var res = new PersonsBuilder().Build(FIRSTNAME, LASTNAME, AGE, SEX);
+      Assert.IsInstanceOfType(res, typeof(Person));
+    }   
   }
 }
