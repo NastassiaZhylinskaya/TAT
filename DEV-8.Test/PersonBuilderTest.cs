7fe22214322993346a548b389cb79a3919ff2435
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-04-18 12:31:24 +0300

    Add files via upload

---
+++ DEV-8.Test/PersonBuilderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DEV8.Test
+{
+  [TestClass]
+  public class PersonBuilderTest
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
