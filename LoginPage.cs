edfcbe791ef1d9d171b1d01335cb8d642666b6a6
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-05-02 18:02:41 +0300

    Add files via upload

---
+++ LoginPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Dev9
+{
+  class LoginPage
+  {
+    IWebDriver driver = new ChromeDriver();        
+    public void NewsPage(string login, string password)
+    {
+      driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+      
+      //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+      driver.Url = "https://vk.com/";
+      if (driver.Url != "https://vk.com/")
+      {
+        throw new Exception("This is not registration page");
+      }
+      driver.FindElement(By.Id("index_email")).SendKeys(login); 
+      driver.FindElement(By.Id("index_pass")).SendKeys(password + Keys.Enter);          
+    }          
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dev9
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter login: ");
+      string login = Console.ReadLine();
+      Console.WriteLine("Enter password: ");
+      string password = Console.ReadLine();
+
+      LoginPage loginPage = new LoginPage();
+      loginPage.NewsPage(login, password);
+
+      NewsPage newsPage = new NewsPage();
+      newsPage.MessagesPage();
+    }
+  }
+}
