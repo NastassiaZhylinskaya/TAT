a040dab1c41746bdd1a9965b0dc1a9ae02822d94
NastassiaZhylinskaya <36952586+NastassiaZhylinskaya@users.noreply.github.com>
2018-05-21 21:51:58 +0300

    Add files via upload

---
+++ Task-3/FolderFileNames.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileInformation1
+{
+  /// <summary>
+  /// Сlass is designed to search and display information about files in a folder.
+  /// </summary>
+  class FolderFileNames
+  {
+    /// <summary>
+    /// List where all information will be recorded.
+    /// </summary>
+    private List<string> fileNames = new List<string>();
+
+    /// <summary>
+    /// This method for get directory files names.
+    /// </summary>
+    /// <param name="source">Folder path.</param>
+    /// <returns>List fileNames.</returns>
+    public List<string> GetDirectoryFiles(string source)
+    {
+      DirectoryInfo directoryInfo = new DirectoryInfo(source);             
+      
+      foreach (var file in directoryInfo.GetFiles())
+      {        
+        fileNames.Add(file.FullName + " " + file.CreationTime.ToString() + " " + file.Length.ToString());      
+      }
+
+      DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+      foreach (var subDirectory in subDirectories)
+      {
+        GetDirectoryFiles(subDirectory.FullName);        
+      }
+
+      return fileNames;
+    }
+
+    /// <summary>
+    /// This method for output information about files.
+    /// </summary>
+    public void OutputDirectoryFiles()
+    {
+      foreach (var fileName in fileNames)
+      {
+        Console.WriteLine(fileName);
+      }
+    }
+  }
+}
